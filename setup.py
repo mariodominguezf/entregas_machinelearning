1b2e50bb43692094604ca32428873aa1f18f9a93
mariodominguezferrer <dominguezferrermario@gmail.com>
2023-11-02 17:28:42 +0100

    Commit

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     install_requires        = [
                                 'pandas',
                                 'numpy',
-                                'torch',
-                                'random'
+                                'torch'
                                 ]
      )
